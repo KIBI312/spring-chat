4ecb03685369f68ca270d4658d96526cca7f7242
KIBI312 <isken00.kg@gmail.com>
2022-03-29 00:57:33 -0500

    Added two default users(other will be deleted on startup);test:test;test1:test1;

--- src/main/java/com/example/chat/ChatApplication.java
+++ src/main/java/com/example/chat/ChatApplication.java
@@ -1,13 +1,33 @@
 package com.example.chat;
 
+import com.example.chat.entity.User;
+import com.example.chat.repository.UserRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
+	}
+ 
+	// public PasswordEncoder passwordEncoder;
+
+
+	@Bean
+	public CommandLineRunner dataLoader(UserRepository repo, PasswordEncoder passwordEncoder) {
+		return new CommandLineRunner() {
+			@Override
+			public  void run(String... args) throws Exception {
+				repo.save(new User("test@test.su", "test", passwordEncoder.encode("test")));
+				repo.save(new User("test@test.su", "test1", passwordEncoder.encode("test1")));
+			}
+		};
 	}
 
 }
