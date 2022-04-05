b2a05f8b83a78d1238705055ccae1b5968758975
KIBI312 <isken00.kg@gmail.com>
2022-04-05 04:57:50 -0500

    Added message pagination methods

--- src/main/java/com/example/chat/repository/MessageRepository.java
+++ src/main/java/com/example/chat/repository/MessageRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import com.example.chat.entity.Message;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MessageRepository extends CrudRepository<Message, Long> {
     List<Message> findTop30ByChatIdOrderByTimestampDesc(Long id);
-
+    List<Message> findAllByChatId(Long id, Pageable pageable);
 }
